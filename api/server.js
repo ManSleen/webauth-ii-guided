e82d43d3c882fcd72020ad1f3ab398c26c2a57ff
Mike VanSleen <mike.vansleen@gmail.com>
2019-08-20 10:54:00 -0600

    Initial commit

--- api/server.js
+++ api/server.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session');
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
 const server = express();
 
+const sessionOptions = {
+  cookie: {
+    name: "example-cookie",
+    secret: process.env.COOKIE_SECRET || "keep it safe!", // for encryption
+    secure: process.env.COOKIE_SECURE || false, // in production this should be set to true, in development setting to false is fine
+    maxAge: 1000 * 60 * 60, // how long is the session going to last, in ms
+    httpOnly: true, // client JS has no access to the cookie
+    resave: false,
+    saveUninitialized: true
+  }
+}
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionOptions))
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
